15fc0fbe96d7c69d628ee25d183760925c33c6fc
tomo <cho.ipmd.ro@gmail.com>
2019-11-25 14:09:20 +0900

    掲示板完成

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,43 @@
+class BoardsController < ApplicationController
+  before_action :get_target_board, only: %i[show edit update destroy]
+  def index
+    @boards = Board.all
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    board = Board.create(board_params)
+    redirect_to boards_path
+  end
+  
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+    @board.update(board_params)
+    redirect_to @board
+  end
+
+  def destroy
+    
+    @board.delete
+    redirect_to boards_path
+  end
+  private
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
+  end
+
+  def get_target_board
+    @board = Board.find(params["id"])
+  end
+end
